e3b150dccce9bc86de3774b130d2dfbca473ce7e
praneethb7 <praneeth.24bcs10081@sst.scaler.com>
2025-02-09 22:36:13 +0530

    Initial Commit

---
+++ utils.js
@@ -0,0 +1,10 @@
+//utils.js
+export async function getActiveTabURL() {
+    const tabs = await chrome.tabs.query({
+        currentWindow: true,
+        active: true
+    });
+  
+    return tabs[0];
+}
+
